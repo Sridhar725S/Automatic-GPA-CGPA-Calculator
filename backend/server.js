5f0773ece1a3a4d16bcd506041b3295fb2fb6f24
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-09 13:36:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -139,11 +139,10 @@ app.get('/api/clicks', async (req, res) => {
   }
 });
 // Serve static files from the Angular build
-
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
-
+app.use(express.static(path.join(__dirname, 'frontend/dist/frontend')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/dist/frontend/index.html'));
 });
+
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
