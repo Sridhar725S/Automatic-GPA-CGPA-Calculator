77c4a8e872e2fbccc85778234bee8351d83361c5
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-11 20:15:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,15 @@ app.get('/api/open-url', async (req, res) => {
   try {
     // Initialize the browser globally
     browser = await puppeteer.launch({
-       headless: 'new',
-      args: [ "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--disable-gpu"],
+       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+    headless: 'new',
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--disable-gpu"
+    ],
         
     })
 
