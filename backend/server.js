d6e3b1d8e6c70763ae0e0487a45ce26441cf89db
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-11 20:11:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,11 @@ app.get('/api/open-url', async (req, res) => {
     // Initialize the browser globally
     browser = await puppeteer.launch({
        headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [ "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--disable-gpu"],
         
     })
 
