cbaf58548b29f242ed626522bac813514251a7ef
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-12 21:33:30 +0530

    Update script.sh

--- backend/script.sh
+++ backend/script.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
-
 # Ensure Playwright is installed
 npx playwright install chromium
 
 # Store/pull Playwright cache with build cache
-if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
-  echo "...Copying Playwright Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+if [[ ! -d "$PLAYWRIGHT_BROWSERS_PATH" ]]; then 
+  echo "...Copying Playwright Cache from Build Cache"
+  mkdir -p "$PLAYWRIGHT_BROWSERS_PATH"
+  cp -R "$XDG_CACHE_HOME/playwright/" "$PLAYWRIGHT_BROWSERS_PATH/"
 else 
-  echo "...Storing Playwright Cache in Build Cache" 
-  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
+  echo "...Storing Playwright Cache in Build Cache"
+  mkdir -p "$XDG_CACHE_HOME/playwright/"
+  cp -R "$PLAYWRIGHT_BROWSERS_PATH/" "$XDG_CACHE_HOME/playwright/"
 fi
