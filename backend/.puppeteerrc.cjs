259a0105599c689e638b7dbacd0ace2cd7488b02
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-14 13:28:03 +0530

    Update .puppeteerrc.cjs

--- backend/.puppeteerrc.cjs
+++ backend/.puppeteerrc.cjs
@@ -5,7 +5,6 @@ const { join } = require("path");
  */
 module.exports = {
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-  
-  // Rename executablePath to path1
-  path1: "/opt/render/.cache/puppeteer/chrome/linux-134.0.6998.35/chrome-linux64/chrome"
+  path1: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome-stable"
 };
+
