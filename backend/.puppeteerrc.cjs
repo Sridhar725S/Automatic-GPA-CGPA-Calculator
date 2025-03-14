33c19c72b9143c451f72587e97e5218c3c890766
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2025-03-14 13:16:56 +0530

    Update .puppeteerrc.cjs

--- backend/.puppeteerrc.cjs
+++ backend/.puppeteerrc.cjs
@@ -1,8 +1,11 @@
-const { join } = require('path');
+const { join } = require("path");
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  
+  // Rename executablePath to path1
+  path1: "/opt/render/.cache/puppeteer/chrome/linux-134.0.6998.35/chrome-linux64/chrome"
 };
